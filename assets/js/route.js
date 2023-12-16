55358ce5c583ec8c275b05f306d66ada7a2b2780
sajjad <sajjadsiadati1390@gmail.com>
2023-12-16 17:23:23 +0330

    add js to route

--- assets/js/route.js
+++ assets/js/route.js
@@ -1,6 +1,23 @@
-/**
- * @license MIT
- * @fileoverview Menage all routes
- * @copyright codewithsadee 2023 All rights reserved
- * @author codewithsadee <mohammadsadee24@gmail.com>
- */
+"use strict";
+import { updateWeather, error404 } from "./app.js";
+const defaultLocation = "#/weather?lat=51.5073219&lon=-0.1276474";
+
+const currentLocation = function () {};
+const searchedLocation = function () {};
+const routes = new Map();
+[["/current-location ,currentLocation "], ["/weather ,searchedLocation "]];
+const checkHash = function () {
+  const requestURL = window.location.hash.slice(1);
+  const [rout, query] = requestURL.includes
+    ? requestURL.split("?")
+    : [requestURL];
+  routes.get(rout) ? routes.get(rout)(query) : error404();
+};
+window.addEventListener("hashchange", checkHash);
+Window.addEventListener("load", function () {
+  if (!window.location.hash) {
+    window.location.hash = "#/current-location";
+  } else {
+    checkHash();
+  }
+});
