6e1e83f72d56b734cd033be7bb22279925fb7ab1
sajjad <sajjadsiadati1390@gmail.com>
2023-12-16 15:18:48 +0330

    add api js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,30 @@
-/**
- * @license MIT
- * @fileoverview All api related stuff like api_key, api request etc.
- * @copyright codewithsadee 2023 All rights reserved
- * @author codewithsadee <mohammadsadee24@gmail.com>
- */
+"use strict";
+
+const api_key = "8c2afaacf65df1976579f048d2fc4e25";
+/** 
+@param {string} URL Api url
+@param {function} callback callback
+*/
+export const fetchData = function (URl, callback) {
+  fetch(`${URL}&appid =${api_key}`)
+    .then((res) => res.json())
+    .then((data) => callback(data));
+};
+
+export const url = {
+  currentWeather(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`;
+  },
+  foreCast(lat, lon) {
+    return `api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`;
+  },
+  airPollution(lat, lon) {
+    return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}&units=metric`;
+  },
+  reverseGeo(lat, lon) {
+    return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`;
+  },
+  geo(query) {
+    return `http://api.openweathermap.org/geo/1.0/direct?q=${London}&limit=5`;
+  },
+};
