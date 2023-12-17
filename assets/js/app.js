fc33477cefda3eaf2805c08e8e176b7eaf8e24f5
sajjad <sajjadsiadati1390@gmail.com>
2023-12-17 23:06:13 +0330

    js app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,12 @@
-/**
- * @license MIT
- * @copyright codewithsadee 2023 All rights reserved
- * @author codewithsadee <mohammadsadee24@gmail.com>
- */
+"use strict";
+
+import { fetchData, url } from "./api.js";
+import * as module from "./module.js";
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (const element of elements) element.addEventListener(eventType, callback);
+};
+const searchView = document.querySelector(["data-search-view"]);
+const searchTogglers = document.querySelectorAll(["data-search-toggler"]);
+const toggleSearch = () => searchView.classList.toggle("active");
+addEventOnElements(searchTogglers, "click", toggleSearch);
