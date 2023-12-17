84750572808fc366ca0bf93aa234eb825855218a
sajjad <sajjadsiadati1390@gmail.com>
2023-12-17 23:07:22 +0330

    add route js

--- assets/js/route.js
+++ assets/js/route.js
@@ -2,8 +2,18 @@
 import { updateWeather, error404 } from "./app.js";
 const defaultLocation = "#/weather?lat=51.5073219&lon=-0.1276474";
 
-const currentLocation = function () {};
-const searchedLocation = function () {};
+const currentLocation = function () {
+  window.navigator.geolocation.getCurrentPosition(
+    (res) => {
+      const { latitude, longitude } = res.coords;
+      updateWeather(`lat = ${latitude}`, `lon = ${longitude}`);
+    },
+    (err) => {
+      window.location.hash = defaultLocation;
+    }
+  );
+};
+const searchedLocation = (query) => updateWeather(...query.split("&"));
 const routes = new Map();
 [["/current-location ,currentLocation "], ["/weather ,searchedLocation "]];
 const checkHash = function () {
