e53dc59d18d1cd959f9179a002c7f50e7d857c15
sajjad <sajjadsiadati1390@gmail.com>
2023-12-21 22:11:30 +0330

    add final api js

--- assets/js/api.js
+++ assets/js/api.js
@@ -16,7 +16,7 @@ export const url = {
     return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`;
   },
   foreCast(lat, lon) {
-    return `api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`;
+    return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`;
   },
   airPollution(lat, lon) {
     return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}&units=metric`;
@@ -28,5 +28,3 @@ export const url = {
     return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`;
   },
 };
-// https://api.openweathermap.org/data/2.5/weather?lat=6.6517883&lon=-10.5931565&appid=8c2afaacf65df1976579f048d2fc4e25
-// https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=8c2afaacf65df1976579f048d2fc4e25
