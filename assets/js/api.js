e4579f9f667c48f6bb464db5be18ab108a5f5f94
sajjad saidati <113238464+SMsajjadSM@users.noreply.github.com>
2023-12-22 00:44:05 +0330

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -19,12 +19,12 @@ export const url = {
     return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`;
   },
   airPollution(lat, lon) {
-    return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}&units=metric`;
+    return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}&units=metric`;
   },
   reverseGeo(lat, lon) {
-    return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`;
+    return `https://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`;
   },
   geo(query) {
-    return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`;
+    return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`;
   },
 };
